5f61bf0f7791b335ae92d7076b55e04f70751118
Shubham Singh <201652026@iiitvadodara.ac.in>
2019-06-20 17:52:07 +0530

    some changes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Internship
+# Admin panel
---
+++ server.js
@@ -0,0 +1,63 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var passport = require("passport");
+var config = require("./config");
+var index = require("./routes/index");
+var users = require("./routes/users");
+var developerRouter = require("./routes/developersRouter");
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, { useNewUrlParser: true });
+
+connect.then(
+  db => {
+    console.log("Connected correctly to server");
+  },
+  err => {
+    console.log(err);
+  }
+);
+
+var app = express();
+app.set({ useFindAndModify: false });
+mongoose.set("useCreateIndex", true);
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Passport middleware
+app.use(passport.initialize());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", index);
+app.use("/users", users);
+app.use("/developers", developerRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+module.exports = app;
